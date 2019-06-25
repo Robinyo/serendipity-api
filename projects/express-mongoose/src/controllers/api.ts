8adb1c59bf967f25804a04353cd8e25fa41cd3b3
Rob Ferguson <rob.ferguson@robferguson.org>
2019-06-25 12:10:54 +1000

    Initial commit

---
+++ projects/express-mongoose/src/controllers/api.ts
@@ -0,0 +1,31 @@
+"use strict";
+
+import graph from "fbgraph";
+import { Response, Request, NextFunction } from "express";
+
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+export const getApi = (req: Request, res: Response) => {
+  res.render("api/index", {
+    title: "API Examples"
+  });
+};
+
+/**
+ * GET /api/facebook
+ * Facebook API example.
+ */
+export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.user.tokens.find((token: any) => token.kind === "facebook");
+  graph.setAccessToken(token.accessToken);
+  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
+    if (err) { return next(err); }
+    res.render("api/facebook", {
+      title: "Facebook API",
+      profile: results
+    });
+  });
+};
