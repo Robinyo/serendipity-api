156b5351fa191d5c1c3ca336b51ffd3d1f8af96d
Rob Ferguson <rob.ferguson@robferguson.org>
2020-06-08 14:14:27 +1000

    Added support for JasperReports Server

--- projects/spring-boot/server/services/jasperreports-server/scripts/wait-for-container-to-exit.sh
+++ projects/spring-boot/server/services/jasperreports-server/scripts/wait-for-container-to-exit.sh
@@ -3,26 +3,34 @@
 
 CONTAINER=$1
 
-API_SERVER=http:/v1.24
-CONTENT_TYPE="Content-Type: application/json"
+# When you're curling the unix socket, you only need a single /
+# API_SERVER=http:/v1.24
+API_SERVER=http:/docker
 
-INSPECT_CONTAINER=`curl --silent --unix-socket /var/run/docker.sock -H ${CONTENT_TYPE} ${API_SERVER}/containers/${CONTAINER}/json`
+INSPECT_CONTAINER=`curl --silent --unix-socket /var/run/docker.sock ${API_SERVER}/containers/${CONTAINER}/json`
 
 INSPECT_CONTAINER_RESULT=$(jq -s --raw-output '.[0].message' <<< "${INSPECT_CONTAINER}")
 
-# echo $INSPECT_CONTAINER | jq
-# echo $INSPECT_CONTAINER_RESULT
+# No such container: jasperreports-server-cmdline
 
-if [[ "${INSPECT_CONTAINER_RESULT}" == "page not found" ]]; then
-  echo "No such container: ${CONTAINER}"
-else
-  echo $INSPECT_CONTAINER | jq
-fi
+while :
+do
+
+  if [[ "${INSPECT_CONTAINER_RESULT}" == *container:* ]]; then
+    echo $INSPECT_CONTAINER_RESULT
+    exit
+  else
+    # echo $INSPECT_CONTAINER | jq
+    echo "sleep 15"
+    sleep 15
+  fi
+
+done
+
+
 
 # chmod 755 wait-for-container-to-exit.sh
 # ./wait-for-container-to-exit.sh jasperreports-server-cmdline
-
-
 
 # https://docs.docker.com/engine/api/v1.24/#31-containers
 # https://docs.docker.com/engine/api/v1.24/
